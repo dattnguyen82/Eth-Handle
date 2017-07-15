9460bd74b27a0d80b98db76497104a73c2537b29
Dat Nguyen <dattnguyen82@gmail.com>
2017-07-14 17:03:48 -0700

    Add files via upload
    Adding source

---
+++ Handle.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.4.0;
+
+
+contract Handle {
+
+  address creator;
+  address holder;
+  string data;
+  uint limit;
+  bool dataSet;
+  uint created;
+  uint passTime;
+
+  event HandlePassed(address newHolder, uint limit, uint passTime);
+
+  function Handle(){
+    creator = msg.sender;
+    dataSet = false;
+    limit = 5;
+  }
+
+  function setData(string newData)
+  {
+    require(msg.sender == creator);
+    require(!dataSet);
+    data = newData;
+    dataSet = true;
+    created = now;
+  }
+
+  function readData() constant returns (string)
+  {
+    require(msg.sender == holder);
+    return data;
+  }
+
+  function pass(address newHolder)
+  {
+    require(msg.sender == holder);
+    _pass(newHolder);
+  }
+
+  //Creator overrides
+  function auditData() constant returns (string)
+  {
+    require(msg.sender == creator);
+    return data;
+  }
+
+  function overridePass(address newHolder) constant returns (string)
+  {
+    require(msg.sender == creator);
+    _pass(newHolder);
+  }
+
+  //internal functions
+  function _pass(address newHolder) internal
+  {
+    require(limit > 0);
+    limit--;
+    holder = newHolder;
+    HandlePassed(holder, limit, now);
+  }
+
+}
